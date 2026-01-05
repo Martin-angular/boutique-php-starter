75bb92a64286826a6c329d1836a642f1a29c947a
Martin <martin@vaud.com>
2026-01-05 17:29:24 +0100

    Mon exercice php

---
+++ exercices/jour-01/hello.php
@@ -0,0 +1,40 @@
+
+<?php
+
+$nom = "Salut PHP";
+
+echo " $nom";
+
+
+
+
+//ok on cree des variables //
+
+
+$name ="Martin";
+$price = 30.15;
+$stock = 30;
+$onSale = true;
+
+var_dump($onSale);
+var_dump($name);
+var_dump($price);
+var_dump($stock);
+
+
+echo "Le prix est de $price €";
+
+
+ //Questions à te poser : Quelle différence vois-tu entre var_dump($price) quand $price = "29.99" vs $price = 29.99 ?
+// reponse : difference ici : c'est un string "" , 
+
+
+    $priceExcludingTax = 100;
+    $vat = 20 ;
+    $quantity = 3;
+
+
+
+
+
+?>
